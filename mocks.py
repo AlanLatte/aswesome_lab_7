8681edbf5b2cee1a3b3db6829f3b3a7411c48561
alanlatte <tNv8YVz4zvbmEEv>
2020-11-27 03:26:35 +0300

    [+] Обновил mock-значения

--- mocks.py
+++ mocks.py
@@ -8,33 +8,33 @@ def mock_trains() -> models.Trains:
         [
             models.Train(
                 number="1",
-                station="Пушкина",
-                time=datetime.datetime(year=2020, month=10, day=12, hour=12, minute=0),
+                station="Кузьминки",
+                time=datetime.datetime(year=2020, month=10, day=12, hour=9, minute=30),
             ),
             models.Train(
                 number="6",
-                station="Пушкина",
-                time=datetime.datetime(year=2020, month=10, day=12, hour=12, minute=0),
+                station="Кузьминки",
+                time=datetime.datetime(year=2020, month=10, day=12, hour=10, minute=45),
             ),
             models.Train(
                 number="2",
-                station="Пушкина",
-                time=datetime.datetime(year=2020, month=10, day=12, hour=13, minute=0),
+                station="Одинцово",
+                time=datetime.datetime(year=2020, month=10, day=12, hour=12, minute=20),
             ),
             models.Train(
                 number="3",
-                station="Пушкина",
-                time=datetime.datetime(year=2020, month=10, day=12, hour=14, minute=0),
+                station="Сколково",
+                time=datetime.datetime(year=2020, month=10, day=12, hour=9, minute=0),
             ),
             models.Train(
                 number="4",
-                station="Пушкина",
-                time=datetime.datetime(year=2020, month=10, day=12, hour=16, minute=0),
+                station="Рабочий посёлок",
+                time=datetime.datetime(year=2020, month=10, day=12, hour=9, minute=20),
             ),
             models.Train(
                 number="5",
-                station="Пушкина",
-                time=datetime.datetime(year=2020, month=10, day=12, hour=15, minute=0),
+                station="Одинцово",
+                time=datetime.datetime(year=2020, month=10, day=12, hour=13, minute=0),
             ),
         ]
     )
